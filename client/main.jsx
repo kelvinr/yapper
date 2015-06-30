f49e3ec3cd25725ed8416ebf66ef0cd29b22ee70
kelvinr <kelvin060@gmail.com>
2015-06-29 22:11:04 -0400

    Setup API and packages

---
+++ client/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import App from './components/layout/App';
+
+React.render(<App />, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: ['./client/main.jsx'],
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'react-hot!babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
