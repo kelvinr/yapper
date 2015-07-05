6918d160e33af522b8cd33f9cdedf51928f503e2
kelvinr <kelvin060@gmail.com>
2015-07-05 10:46:28 -0400

    Adjust Jsonify module

--- lib/jsonify.rb
+++ lib/jsonify.rb
@@ -6,10 +6,18 @@ module Jsonify
 
   module InstanceMethods
     def jsonify(*args)
-      connection.select_all(select([*args]).arel, nil, all.bind_values).each do |attrs|
-        attrs.each do |name, value|
-          attrs[name] = column_types[name].send(:type_cast_from_database, value)
+      ar = args.last
+      if ar.is_a?(Hash)
+        key = ar.keys[0]
+        args.delete(ar)
+        if ar[key].include?(",")
+          assoc = {key => self.send(key).attributes.keep_if{|c| ar[key].include?(c)}}
+        else
+          assoc = {key => self.send(key).send(ar[key])}
         end
+        Hash[args.zip(args.map{|c| self.send(c)})].merge(assoc)
+      else
+        Hash[args.zip(args.map{|c| self.send(c)})]
       end
     end
   end
